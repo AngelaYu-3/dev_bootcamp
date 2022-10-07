b34b661dd5ddcd3ecc47b96386917ba91438c0df
AngelaYu-3 <yuangela12@gmail.com>
2022-10-07 00:43:10 -0400

    added comments to js file

--- mid_sem_proj/script.js
+++ mid_sem_proj/script.js
@@ -1,7 +1,6 @@
 const httpAdd = "https://pokeapi.co/api/v2/pokemon/"
-const infoButton = document.getElementById("infoButton")
-const movesButton = document.getElementById("movesButton")
 
+//key-value pair for type color
 const colorType = {
     normal: "A8A77A",
     fire: "EE8130",
@@ -23,17 +22,18 @@ const colorType = {
     fairy: "D685AD"
 };
 
+// global variables 
 let pokeNum = 1
 let isInfo = true
 let isMoves = false
 
-infoButton.textContent = "info"
-movesButton.textContent = "moves"
-
+// main async / Promise function
 async function getPokemonData(pokeNum) {
+    // fetching
     const response = await fetch(httpAdd + pokeNum)
     const data = await response.json()
 
+    // getting all needed data
     let name = data["name"]
     let image = data["sprites"]["other"]["official-artwork"]["front_default"]
     let moves = data["moves"]
@@ -47,56 +47,56 @@ async function getPokemonData(pokeNum) {
     let special_defense = "special-defense: " + data["stats"][4]["base_stat"]
     let speed = "speed: " + data["stats"][5]["base_stat"]
 
+    // adding pokemon name and image
     document.getElementById("nameButton").innerText = name
     document.getElementById("pokeImage").src = image
 
-    document.getElementById("typeButtons").innerHTML = "";
+    // clearing prior type buttons and adding type buttons w/ appropriate colors
+    document.getElementById("typeButtons").innerHTML = ""
     for(i = 0; i < types.length; i++){
         let name = types[i]["type"]["name"];
-        document.getElementById("typeButtons").innerHTML += "<button style='background-color: #" + colorType[name] + ";'" + "id=typeButton>" + name + "</button>";
+        document.getElementById("typeButtons").innerHTML += "<button style='background-color: #" + colorType[name] + ";'" + "id=typeButton>" + name + "</button>"
     }
 
+    //Info + Moves button logic
     if(isInfo){
+        // changing right header to info + adding necessary text
         document.getElementById("rightText").innerText = "info"
-        document.getElementById("height").innerText = height
-        document.getElementById("weight").innerText = weight
-        document.getElementById("hp").innerText = hp
-        document.getElementById("attack").innerText = attack
-        document.getElementById("defense").innerText = defense
-        document.getElementById("special_attack").innerText = special_attack
-        document.getElementById("special_defense").innerText = special_defense
-        document.getElementById("speed").innerText = speed
+        addInfo(height, weight, hp, attack, defense, special_attack, special_defense, speed);
 
-        document.getElementById("moves").style.width = "0px";
-        document.getElementById("moves").style.height = "0px";
-        document.getElementById("moves").style.overflow = "normal";
+        // clearing scroll feature
+        document.getElementById("moves").style.width = "0px"
+        document.getElementById("moves").style.height = "0px"
+        document.getElementById("moves").style.overflow = "normal"
     } else{
+        // changing right header to moves + clearing prior info text
         document.getElementById("rightText").innerText = "moves"
-        document.getElementById("height").innerText = ""
-        document.getElementById("weight").innerText = ""
-        document.getElementById("hp").innerText = ""
-        document.getElementById("attack").innerText = ""
-        document.getElementById("defense").innerText = ""
-        document.getElementById("special_attack").innerText = ""
-        document.getElementById("special_defense").innerText = ""
-        document.getElementById("speed").innerText = ""
+        clearInfo();
 
-        document.getElementById("moves").innerHTML = "";
+        // clearing prior listed moves on right side + adding new moves for new pokemon
+        document.getElementById("moves").innerHTML = ""
         for(i = 0; i < moves.length; i++){ 
-            document.getElementById("moves").innerHTML += "<div>" + moves[i]["move"]["name"] + "</div>";
+            document.getElementById("moves").innerHTML += "<div>" + moves[i]["move"]["name"] + "</div>"
         }
 
-        document.getElementById("moves").style.width = "300px";
-        document.getElementById("moves").style.height = "550px";
-        document.getElementById("moves").style.overflow = "auto";
+        // adding scroll feature
+        document.getElementById("moves").style.width = "300px"
+        document.getElementById("moves").style.height = "550px"
+        document.getElementById("moves").style.overflow = "auto"
     }
     
 }
 
+// buttons 
 const arrowButton1 = document.getElementById("arrowButton1")
 const arrowButton2 = document.getElementById("arrowButton2")
-getPokemonData(1)
+const infoButton = document.getElementById("infoButton")
+const movesButton = document.getElementById("movesButton")
+infoButton.textContent = "info"
+movesButton.textContent = "moves"
+getPokemonData(pokeNum)
 
+// infoButton
 infoButton.addEventListener("click", () => {
     isInfo = true
     isMoves = false
@@ -106,6 +106,7 @@ infoButton.addEventListener("click", () => {
     getPokemonData(pokeNum)
 });
 
+// movesButton
 movesButton.addEventListener("click", () => {
     isInfo = false
     isMoves = true
@@ -116,14 +117,38 @@ movesButton.addEventListener("click", () => {
     getPokemonData(pokeNum)
 });
 
+// backward arrowButton
 arrowButton1.addEventListener("click", () => {
     --pokeNum
     if(pokeNum < 1) pokeNum = 905
     getPokemonData(pokeNum)
 });
 
+// forward arrowButton
 arrowButton2.addEventListener("click", () => {
     ++pokeNum
     if(pokeNum > 905) pokeNum = 1
     getPokemonData(++pokeNum)
 });
+
+function clearInfo(){
+    document.getElementById("height").innerText = ""
+    document.getElementById("weight").innerText = ""
+    document.getElementById("hp").innerText = ""
+    document.getElementById("attack").innerText = ""
+    document.getElementById("defense").innerText = ""
+    document.getElementById("special_attack").innerText = ""
+    document.getElementById("special_defense").innerText = ""
+    document.getElementById("speed").innerText = ""
+}
+
+function addInfo(height, weight, hp, attack, defense, special_attack, special_defense, speed){
+    document.getElementById("height").innerText = height
+    document.getElementById("weight").innerText = weight
+    document.getElementById("hp").innerText = hp
+    document.getElementById("attack").innerText = attack
+    document.getElementById("defense").innerText = defense
+    document.getElementById("special_attack").innerText = special_attack
+    document.getElementById("special_defense").innerText = special_defense
+    document.getElementById("speed").innerText = speed
+}
